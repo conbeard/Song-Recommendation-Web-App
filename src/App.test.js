bfdca20b5304b53ccb9f2bdc1b41e0dd4db197ca
conbeard <conbeard21@gmail.com>
2021-10-28 22:35:09 -0700

    Starter files

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Wave at Me/i);
   expect(linkElement).toBeInTheDocument();
 });
